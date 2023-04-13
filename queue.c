4ef115c71f45f195d03d3f4303a288aa4505f94f
Aymane El Jerari <m.aymane.eljerari@gmail.com>
2023-04-13 17:09:59 -0400

    added queue structure functionality

---
+++ queue.c
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "constants.h"
+#include "queue.h"
+
+/* Queue operations */
+void initQueue(Queue *q) {
+    q->front = q->rear = -1;
+}
+
+bool isQueueEmpty(Queue *q) {
+    return (q->front == -1 && q->rear == -1);
+}
+
+bool isQueueFull(Queue *q) {
+    return ((q->rear+1)%MAX_QUEUE_SIZE == q->front);
+}
+
+void enqueue(Queue *q, int val) {
+    if (isQueueFull(q)) {
+        printf("Queue overflow\n");
+        return;
+    } else if (isQueueEmpty(q)) {
+        q->front = q->rear = 0;
+    } else {
+        q->rear = (q->rear+1)%MAX_QUEUE_SIZE;
+    }
+    q->data[q->rear] = val;
+}
+
+int dequeue(Queue *q) {
+    int val;
+    if (isQueueEmpty(q)) {
+        printf("Queue underflow\n");
+        return -1;
+    } else if (q->front == q->rear) {
+        val = q->data[q->front];
+        q->front = q->rear = -1;
+    } else {
+        val = q->data[q->front];
+        q->front = (q->front+1)%MAX_QUEUE_SIZE;
+    }
+    return val;
+}
