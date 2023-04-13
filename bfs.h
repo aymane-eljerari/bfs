c0deea31b70a90dd90b379476f5bf04fff8f054b
Aymane El Jerari <m.aymane.eljerari@gmail.com>
2023-04-13 17:12:48 -0400

    added header files

---
+++ bfs.h
@@ -0,0 +1,12 @@
+#ifndef BFS_H
+#define BFS_H
+
+#include "graph.h"
+#include "queue.h"
+
+void bfs(Graph *g, int startVertex);
+
+#endif
+
+
+
---
+++ constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_QUEUE_SIZE 100
+
+#endif
