35d158340ad0e4186e26ea8a2b5db6a2a82e6554
Aymane El Jerari <m.aymane.eljerari@gmail.com>
2023-04-13 17:34:36 -0400

    modified main.c

--- main.c
+++ main.c
@@ -10,24 +10,11 @@
 /* Test the algorithm */
 int main() {
     Graph g;
-    int vertexCount = 5;
+    int vertexCount = 10;
     int maxDegree = 3;
 
     generate(&g, vertexCount, maxDegree);
     printAdjacencyMatrix(&g);
-
-    // initGraph(&g, vertexCount);
-
-    // addEdge(&g, 0, 1);
-    // addEdge(&g, 0, 2);
-    // addEdge(&g, 1, 3);
-    // addEdge(&g, 1, 4);
-    // addEdge(&g, 2, 4);
-    // addEdge(&g, 3, 4);
-    // addEdge(&g, 3, 5);
-
-    // printf("BFS Traversal: ");
-    // bfs(&g, 0);
 
     return 0;
 }
